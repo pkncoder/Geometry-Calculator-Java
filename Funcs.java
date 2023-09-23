bdba1526e191c2ca7d6beed711b7bdd1e9c6e3cf
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-09-23 14:34:51 +0000

    Added constructors in the function file

---
+++ Funcs.java
@@ -0,0 +1,29 @@
+public class Funcs {
+    // Initalize variables that we will need
+    static double xOne;
+    static double yOne;
+    static double xTwo;
+    static double yTwo;
+    static int topRatio;
+    static int bottomRatio;
+
+    // Make the constructors for everything needed
+    // The constructor for ratio ones
+    public Funcs(double firstX, double firstY, double secondX, double secondY, int ratioPartOne, int ratioPartTwo) {
+        xOne = firstX;
+        yOne = firstY;
+        xTwo = secondX;
+        yTwo = secondY;
+        topRatio = ratioPartOne;
+        bottomRatio = ratioPartTwo;
+    }
+
+    // The constructor for the normal ones, as just the coordinates
+    public Funcs(double firstX, double firstY, double secondX, double secondY) {
+        xOne = firstX;
+        yOne = firstY;
+        xTwo = secondX;
+        yTwo = secondY;
+    }
+
+}
